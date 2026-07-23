ca1dadd6f501c69a215c066e287d94efc9a2feab
Misato Seki <misatoseki@MisatonoMacBook-Pro.local>
2026-07-23 13:05:22 +0900

    feat: display quiz question one at a time

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient, SelectionSet } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-// import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import MemberCard from "@/components/MemberCard";
 import WelcomeMessage from "@/components/WelcomeMessage";
 
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
-
-const selectionSet = ["id", "content", "description", "choices.*"] as const;
-type QuizzesWithChoices = SelectionSet<
-  Schema["Quiz"]["type"],
-  typeof selectionSet
->;
-
 export default function App() {
   // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [quizzes, setQuizzes] = useState<Array<QuizzesWithChoices>>([]);
 
   // function deleteTodo(id: string) {
   //   client.models.Todo.delete({ id })
@@ -34,20 +17,6 @@ export default function App() {
   //   });
   // }
 
-  function listQuizzes() {
-    client.models.Quiz.observeQuery({
-      selectionSet: ["id", "content", "description", "choices.*"],
-    }).subscribe({
-      next: (data) => setQuizzes([...data.items]),
-    });
-  }
-  console.log("quizzes", quizzes);
-
-  useEffect(() => {
-    // listTodos();
-    listQuizzes();
-  }, []);
-
   // function createTodo() {
   //   client.models.Todo.create({
   //     content: window.prompt("Todo content"),
@@ -58,44 +27,6 @@ export default function App() {
     <div>
       <WelcomeMessage />
       <MemberCard />
-      {/* <h1>My Quizzes</h1> */}
-      {/* <button onClick={createTodo}>+ new</button> */}
-      {/* <ul>
-        {quizzes.map((quiz) => (
-          <li
-            // onClick={() => deleteTodo(todo.id)}
-            key={quiz.id}
-          >
-            <p>
-              <strong className="text-4xl font-bold">質問:</strong>{" "}
-              {quiz.content}
-            </p>
-            {quiz.choices &&
-              quiz.choices.length > 0 &&
-              quiz.choices.map((choice) => (
-                <div key={choice.id}>
-                  <p>
-                    <strong>選択肢:</strong> {choice.content}
-                  </p>
-                  <p>
-                    <strong>解答:</strong>{" "}
-                    {choice.answer === true ? "⭕️" : "❌"}
-                  </p>
-                </div>
-              ))}
-            <p>
-              <strong>説明:</strong> {quiz.description}
-            </p>
-          </li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div> */}
     </div>
   );
 }
--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,3 +1,104 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient, SelectionSet } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+import { Button } from "@/components/ui/button";
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+const selectionSet = ["id", "content", "description", "choices.*"] as const;
+type QuizWithChoices = SelectionSet<
+  Schema["Quiz"]["type"],
+  typeof selectionSet
+>;
+
 export default function QuizPage() {
-  return <p>クイズページです</p>;
+  const [quizzes, setQuizzes] = useState<Array<QuizWithChoices>>([]);
+  const [displayedQuiz, setDisplayedQuiz] = useState<QuizWithChoices | null>(
+    quizzes.length > 0 ? quizzes[0] : null,
+  );
+  const [isFinished, setIsFinished] = useState<boolean>(false);
+
+  function insertQuizzes(quizzes: Array<QuizWithChoices>) {
+    if (quizzes.length > 0) {
+      setQuizzes(quizzes);
+      setDisplayedQuiz(quizzes[0]);
+    } else {
+      setQuizzes([]);
+      setDisplayedQuiz(null);
+    }
+  }
+
+  function listQuizzes() {
+    client.models.Quiz.observeQuery({
+      selectionSet: ["id", "content", "description", "choices.*"],
+    }).subscribe({
+      next: (data) => insertQuizzes([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listQuizzes();
+  }, []);
+
+  function incrementDisplayedQuiz() {
+    const currentIndex = quizzes.findIndex(
+      (quiz) => quiz.id === displayedQuiz?.id,
+    );
+    if (currentIndex === quizzes.length - 1) {
+      setIsFinished(true);
+      return;
+    } else {
+      const nextIndex = currentIndex + 1;
+      setDisplayedQuiz(quizzes[nextIndex]);
+    }
+  }
+
+  return (
+    <>
+      {isFinished ? (
+        <div>
+          <h2>クイズ終了!</h2>
+          <p>お疲れ様でした!</p>
+        </div>
+      ) : (
+        <div>
+          {displayedQuiz && (
+            <div>
+              <p>
+                <strong>質問:</strong> {displayedQuiz.content}
+              </p>
+              {displayedQuiz.choices &&
+                displayedQuiz.choices.length > 0 &&
+                displayedQuiz.choices.map((choice) => (
+                  <div key={choice.id}>
+                    <p>
+                      <strong>選択肢:</strong> {choice.content}
+                    </p>
+                    <p>
+                      <strong>解答:</strong>{" "}
+                      {choice.answer === true ? "⭕️" : "❌"}
+                    </p>
+                  </div>
+                ))}
+              <p>
+                <strong>説明:</strong> {displayedQuiz.description}
+              </p>
+            </div>
+          )}
+          {quizzes.length === 0 ? (
+            <p>クイズがありません。</p>
+          ) : (
+            <Button onClick={incrementDisplayedQuiz}>次の問題</Button>
+          )}
+        </div>
+      )}
+    </>
+  );
 }
